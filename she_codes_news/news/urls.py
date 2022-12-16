7b0741ee31d80d74d340fd47caa1447ce1f41c8d
Ben Fitzhardinge <ben@fitzhardinge.net>
2022-12-17 09:33:05 +1100

    added filter and search again

--- she_codes_news/news/urls.py
+++ she_codes_news/news/urls.py
@@ -6,5 +6,6 @@ app_name = 'news'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.StoryView.as_view(), name="story"),
+    # path('<int:pk>/like', views.like_post, name="like"),
     path("add-story/", views.AddStoryView.as_view(), name='newStory'),
 ]
--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -1,19 +1,40 @@
+from django.contrib.auth import get_user_model
+from django.db.models import Q
+from django.shortcuts import get_object_or_404
 from django.views import generic
 from django.urls import reverse_lazy
 from .models import NewsStory
-from .forms import StoryForm
+from .forms import StoryForm, FilterForm
 
+User = get_user_model()
 class IndexView(generic.ListView):
     template_name = 'news/index.html'
+    context_object_name = 'all_stories'
 
     def get_queryset(self):
         '''Return all news stories.'''
-        return NewsStory.objects.all()
+        qs = NewsStory.objects.all()
+        form = FilterForm(self.request.GET)
+        order_by = "-pub_date"
+        if form.is_valid():
+            order = form.cleaned_data.get('order')
+            if order == "oldfirst":
+                order_by = "pub_date"
+                
+
+            if author := form.cleaned_data.get('author'):
+                qs = qs.filter(author=author)
+
+            if search := form.cleaned_data.get('search'):
+                qs = qs.filter(Q(title__icontains=search) | Q(content__icontains=search))
+
+
+        return qs.order_by(order_by)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['latest_stories'] = NewsStory.objects.all()[:4]
-        context['all_stories'] = NewsStory.objects.all()
+        # context['latest_stories'] = NewsStory.objects.all()[:4]
+        context['form'] = FilterForm(self.request.GET)
         return context
 
 class StoryView(generic.DetailView):
